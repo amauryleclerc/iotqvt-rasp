3da52be66c97b08b2db48ffded08f2e89ba169b5
leludo044 <leludo044@gmail.com>
2015-07-09 19:52:13 +0200

    Enrichissement des logs de WebsocketClient

--- src/main/java/fr/iotqvt/rasp/infra/websocket/WebsocketClient.java
+++ src/main/java/fr/iotqvt/rasp/infra/websocket/WebsocketClient.java
@@ -7,12 +7,14 @@ import javax.websocket.ClientEndpoint;
 import javax.websocket.CloseReason;
 import javax.websocket.ContainerProvider;
 import javax.websocket.DeploymentException;
-import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import fr.iotqvt.rasp.modele.IOT;
 import fr.iotqvt.rasp.modele.Mesure;
@@ -20,81 +22,77 @@ import fr.iotqvt.rasp.modele.Mesure;
 @ClientEndpoint
 public class WebsocketClient {
 
-    private Session session = null;
-    private URI endpointURI = null;
-    private IOT cedec ;
-    
-    public WebsocketClient(URI endpointURI, IOT cedec) {
-    	this.endpointURI = endpointURI;
-    	this.cedec = cedec ;
-    }
-    private void connect() throws DeploymentException, IOException{
-        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-        container.connectToServer(this, endpointURI);
-        // Identification à chaque connection
-        this.sendIdentification();
-    }
-  
-    @OnOpen
-    public void onOpen(Session userSession) {
-        System.out.println("opening websocket");
-        this.session = userSession;
-    }
+	private static Logger LOG = LogManager.getLogger(WebsocketClient.class);
 
-   
-    @OnClose
-    public void onClose(Session userSession, CloseReason reason) {
-        System.out.println("closing websocket");
-        this.session = null;
-    }
+	private Session session = null;
+	private URI endpointURI = null;
+	private IOT cedec;
 
-    
-    @OnMessage
-    public void onMessage(String message) {
-    	// System.out.println("message : "+message);
-    }
+	public WebsocketClient(URI endpointURI, IOT cedec) {
+		this.endpointURI = endpointURI;
+		this.cedec = cedec;
+	}
 
- 
-    public void sendMessage(String message) {
-        this.session.getAsyncRemote().sendText(message);
-    }
+	private void connect() throws DeploymentException, IOException {
+		LOG.info("Tentative de connexion au master "+endpointURI.toString());
+		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+		container.connectToServer(this, endpointURI);
+		// Identification à chaque connection
+		this.sendIdentification();
+	}
 
+	@OnOpen
+	public void onOpen(Session userSession) {
+		LOG.info("Connexion au master établie.");
+		this.session = userSession;
+	}
 
-    public synchronized  boolean   sendMesure(Mesure m) {
-    	MesureMessage msg = new MesureMessage(m);
-    	System.out.println(new MesureMessage(m));
-    	if(session == null){
-    		 try {
-				connect() ;
+	@OnClose
+	public void onClose(Session userSession, CloseReason reason) {
+		LOG.error("Connexion au master perdue !");
+		this.session = null;
+	}
+
+	@OnMessage
+	public void onMessage(String message) {
+		// LOG.warn("Message reçu !");
+	}
+
+	public synchronized boolean sendMesure(Mesure m) {
+		MesureMessage msg = new MesureMessage(m);
+		if (session == null) {
+			try {
+				connect();
 				this.session.getAsyncRemote().sendObject(msg);
 				return true;
 			} catch (DeploymentException | IOException e) {
-				System.out.println("connexion impossible "+e.getMessage());
+				LOG.error("Connexion au master impossible !", e);
 				return false;
 			}
-    	}else{
-    		this.session.getAsyncRemote().sendObject(msg);
-    		return true;
-    	}
-    	
-    }
+		} else {
+			this.session.getAsyncRemote().sendObject(msg);
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Mesure envoyée : " + msg);
+			}
+			return true;
+		}
 
-    public synchronized  void sendIdentification() {
-    	IdentificationMessage msg = new IdentificationMessage(cedec);
-    	System.out.println(msg);
-    	if(session == null){
-    		 try {
-				connect() ;
+	}
+
+	public synchronized void sendIdentification() {
+		IdentificationMessage msg = new IdentificationMessage(cedec);
+		if (session == null) {
+			try {
+				connect();
 				this.session.getAsyncRemote().sendObject(msg);
 			} catch (DeploymentException | IOException e) {
-				System.out.println("connexion impossible "+e.getMessage());
+				LOG.error("Connexion au master impossible !", e);
 			}
-    	}else{
-				this.session.getAsyncRemote().sendObject(msg);
-    	}
-    	
-    }
+		} else {
+			this.session.getAsyncRemote().sendObject(msg);
+			LOG.info("Identification envoyée : " + msg);
+		}
 
-
+	}
 
 }
